a40f0f034c7ae67bacd7f7e3518a4c447e3274c1
Martin Dimitrov <MDI@milestone.dk>
2021-04-11 22:40:22 +0300

    Removed broken project

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -8,19 +8,19 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            var timer = new Utils.Timer();
+            //var timer = new Utils.Timer();
             var sw = new Stopwatch();
             sw.Start();
-            timer.Start();
+            //timer.Start();
             
             Console.WriteLine("Hello World!");
             Thread.Sleep(1000);
             
-            timer.Stop();
+            //timer.Stop();
             sw.Stop();
-            var duration = timer.GetDuration();
+            //var duration = timer.GetDuration();
             
-            Console.WriteLine($"[Timer] Hello world took {duration} to execute");
+            //Console.WriteLine($"[Timer] Hello world took {duration} to execute");
             Console.WriteLine($"[StopWatch] Hello world took {sw.Elapsed} to execute");
             Console.ReadLine();
         }
