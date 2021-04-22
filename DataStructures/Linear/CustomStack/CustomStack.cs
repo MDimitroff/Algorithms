36ba906d470b81c87d8a5504c548f00ed0ccee9f
Martin Dimitrov <MDI@milestone.dk>
2021-04-22 23:13:41 +0300

    Added custome implementation of stack.

---
+++ DataStructures/Linear/CustomStack/CustomStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class CustomStack<T>
+    {
+        private int index;
+        private readonly double resizeFactor;
+        private int resizeIteration;
+        private T[] array;
+
+        public int Lenght => index + 1;
+
+        public CustomStack()
+        {
+            index = -1;
+            resizeFactor = 1.5;
+            resizeIteration = 0;
+            array = new T[10];
+        }
+
+        public void Push(T element)
+        {
+            if (index + 1 > array.Length - 1)
+            {
+                Resize();
+            }
+
+            index++;
+            array[index] = element;
+        }
+
+        public T Pop()
+        {
+            var element = array[index];
+            array[index] = default;
+            index--;
+
+            return element;
+        }
+
+        public T Peek()
+        {
+            return array[index];
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i <= index; i++)
+            {
+                array[i] = default;
+            }
+
+            index = -1;
+            resizeIteration = 0;
+            Resize();
+        }
+
+        private void Resize()
+        {
+            resizeIteration++;
+            int currentSize = array.Length;
+            int newSize = (int)Math.Floor(currentSize * (resizeFactor * resizeIteration));
+            var resizedArray = new T[newSize];
+
+            for (int i = 0; i < currentSize; i++)
+            {
+                resizedArray[i] = array[i];
+            }
+
+            array = resizedArray;
+        }
+    }
+}
---
+++ DataStructures/Linear/CustomStack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new CustomStack<string>();
+            stack.Push("Martin");
+            stack.Push("Evgenia");
+            stack.Push("Adrian");
+            stack.Push("Alexandra");
+
+            int stackLenght = stack.Lenght;
+            Console.WriteLine($"Printing stack values. Stack length {stackLenght}");
+
+            for (int i = 0; i < stackLenght; i++)
+            {
+                Console.WriteLine($"{stack.Pop()}");
+            }
+
+            Console.WriteLine($"Operation completed. Stack length {stack.Lenght}");
+        }
+    }
+}
