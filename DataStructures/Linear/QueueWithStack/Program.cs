7da0564918e17eb67fec44b9035083d6376cc455
Martin Dimitrov <MDI@milestone.dk>
2021-06-04 19:46:34 +0300

    Added queue build with stacks

---
+++ DataStructures/Linear/QueueWithStack/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QueueWithStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new QueueWithStack<int>();
+            queue.EnQueue(0);
+            queue.EnQueue(1);
+            queue.EnQueue(2);
+
+            Console.WriteLine($"Values are \n {queue.DeQueue()} \n {queue.DeQueue()} \n {queue.DeQueue()}");
+        }
+    }
+}
---
+++ DataStructures/Linear/QueueWithStack/QueueWithStack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace QueueWithStack
+{
+    public class QueueWithStack<T>
+    {
+        private readonly Stack<T> _stack;
+        public int Count { get; set; }
+
+        public QueueWithStack()
+        {
+            _stack = new Stack<T>();
+            Count = 0;
+        }
+
+        public void EnQueue(T item)
+        {
+            _stack.Push(item);
+            Count++;
+        }
+
+        public T DeQueue()
+        {
+            Stack<T> stack = new Stack<T>();
+
+            while (_stack.Count > 0)
+            {
+                stack.Push(_stack.Pop());
+            }
+
+            T item = stack.Pop();
+            Count--;
+
+            while (stack.Count > 0)
+            {
+                _stack.Push(stack.Pop());
+            }
+
+            return item;
+        }
+    }
+}
