45c8a1f0fcae30749265144d596ea2ecdb8afe7c
Martin Dimitrov <MDI@milestone.dk>
2021-06-07 11:33:50 +0300

    Added encryption and decryption by using XOR

---
+++ Agorithms/Others/EncryptWithXOR/EncryptWithXOR.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace EncryptWithXOR
+{
+    public class EncryptWithXOR
+    {
+        public static byte[] EncryptOrDecrypt(string text, string key)
+        {
+            List<byte> result = new List<byte>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int asciiCodeText = text[i];
+                int keyPosition = i % key.Length;
+                int asciiCodeKey = key[keyPosition];
+
+                // Perform XOR operation on the two characters
+                byte combinedCode = (byte) (asciiCodeText ^ asciiCodeKey);
+                result.Add(combinedCode);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ Agorithms/Others/EncryptWithXOR/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace EncryptWithXOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalText = "Hello World!";
+            string encryptionKey = "Sofia2021";
+            byte[] encryptedText, decryptedText = null;
+            string encryptedTexAsString, decryptedTexAsString = null;
+
+            encryptedText = EncryptWithXOR.EncryptOrDecrypt(originalText, encryptionKey);
+            encryptedTexAsString = Encoding.UTF8.GetString(encryptedText);
+
+            decryptedText = EncryptWithXOR.EncryptOrDecrypt(encryptedTexAsString, encryptionKey);
+            decryptedTexAsString = Encoding.UTF8.GetString(decryptedText);
+
+            Console.WriteLine($"Value in plain text = {originalText}");
+            Console.WriteLine($"Encrypted text = {encryptedTexAsString}");
+            Console.WriteLine($"Decrypted text = {decryptedTexAsString}");
+            Console.WriteLine($"Does decrypted value equal to the plain text's value? {decryptedTexAsString == originalText}");
+        }
+    }
+}
