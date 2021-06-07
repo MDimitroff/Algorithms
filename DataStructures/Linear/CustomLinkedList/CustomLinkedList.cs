4fcb1196da634c753ca3dbd69fed00dec7c9aec0
Martin Dimitrov <MDI@milestone.dk>
2021-06-08 00:09:53 +0300

    Added custom linked list

---
+++ DataStructures/Linear/CustomLinkedList/CustomLinkedList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomLinkedList
+{
+    public class CustomLinkedList<T>
+    {
+        protected Node<T> header;
+        public int Count;
+
+        public CustomLinkedList()
+        {
+            header = new Node<T>(default);
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            var node = Find(item);
+            return node != null;
+        }
+
+        public void Add(T item)
+        {
+            var newNode = new Node<T>(item);
+            Count++;
+            if (header.Link == null)
+            {
+                header.Link = newNode;
+                return;
+            }
+
+            var current = header;
+            while (current.Link != null)
+            {
+                current = current.Link;
+            }
+
+            current.Link = newNode;
+        }
+
+        public void InsertAfter(T item, T after)
+        {
+            var newNode = new Node<T>(item);
+            var afterNode = Find(after);
+            newNode.Link = afterNode.Link;
+            afterNode.Link = newNode;
+            Count++;
+        }
+
+        public void Dump()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"List contains the following { Count } items:");
+            var current = header;
+            while (current.Link != null)
+            {
+                T item = current.Item;
+                current = current.Link;
+
+                if (EqualityComparer<T>.Default.Equals(item, default))
+                    continue;
+
+                Console.WriteLine($"{item}");
+            }
+
+            Console.WriteLine($"{current.Item}");
+            Console.WriteLine();
+        }
+
+        private Node<T> Find(T item)
+        {
+            var currentNode = header;
+            while (!EqualityComparer<T>.Default.Equals(currentNode.Item, item))
+            {
+                currentNode = currentNode.Link;
+            }
+
+            return currentNode;
+        }
+
+        protected class Node<V>
+        {
+            public V Item;
+            public Node<V> Link;
+
+            public Node()
+            {
+                Item = default;
+                Link = null;
+            }
+
+            public Node(V item)
+            {
+                Item = item;
+                Link = null;
+            }
+        }
+    }
+}
---
+++ DataStructures/Linear/CustomLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new CustomLinkedList<string>();
+            list.Add("Sofia");
+            list.Add("Varna");
+            list.Add("Burgas");
+            list.Dump();
+
+            list.InsertAfter("Plovdiv", "Sofia");
+            list.Dump();
+        }
+    }
+}
