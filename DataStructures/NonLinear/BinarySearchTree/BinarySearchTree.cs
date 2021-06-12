dcae512e55c55951ba7d0e27d2ccccd369aadf09
Martin Dimitrov <MDI@milestone.dk>
2021-06-12 20:44:20 +0300

    Added binary search tree

---
+++ DataStructures/NonLinear/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree<T>
+    {
+        public Node<T> RootNode { get; set; }
+
+        public BinarySearchTree()
+        {
+            RootNode = null;
+        }
+
+        public void Insert(T value)
+        {
+            var newNode = new Node<T>();
+            newNode.Value = value;
+            
+            if (RootNode == null)
+            {
+                RootNode = newNode;
+                return;
+            }
+
+            // Traverse the tree
+            var current = RootNode;
+            Node<T> parent;
+
+            while (true)
+            {
+                parent = current;
+
+                if (value is int insertedValue)
+                {
+                    int currentNodeValue = int.Parse(current.Value.ToString());
+
+                    if (insertedValue < currentNodeValue)
+                    {
+                        current = current.LeftNode;
+                        if (current == null)
+                        {
+                            parent.LeftNode = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.RightNode;
+                        if (current == null)
+                        {
+                            parent.RightNode = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void DisplayNode(Node<T> node)
+        {
+            Console.WriteLine(node.Value);
+        }
+
+        public Node<T> Find(T item)
+        {
+            Node<T> current = RootNode;
+
+            if (item is int valueToFind)
+            {
+                int currentValue;
+                while ((currentValue = int.Parse(current.Value.ToString())) != valueToFind)
+                {
+                    if (valueToFind < currentValue)
+                    {
+                        current = current.LeftNode;
+                        if (current == null)
+                        {
+                            return null;
+                        }
+                    }
+                    else
+                    {
+                        current = current.RightNode;
+                        if (current == null)
+                        {
+                            return null;
+                        }
+                    }
+                }
+
+                return current;
+            }
+            else
+            {
+                throw new ArgumentException($"The provided value for T is of an invalid type");
+            }
+        }
+
+        public bool Exists(T item)
+        {
+            return Find(item) != null;
+        }
+
+        /// <summary>
+        /// Traverses the tree in an ascending numerical order
+        /// </summary>
+        /// <param name="node"></param>
+        public void InOrder(Node<T> node)
+        {
+            if (node != null)
+            {
+                InOrder(node.LeftNode);
+                DisplayNode(node);
+                InOrder(node.RightNode);
+            }
+        }
+
+        public class Node<V>
+        {
+            public V Value { get; set; }
+            public Node<V> LeftNode { get; set; }
+            public Node<V> RightNode { get; set; }
+        }
+    }
+}
---
+++ DataStructures/NonLinear/BinarySearchTree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(23);
+            bst.Insert(45);
+            bst.Insert(16);
+            bst.Insert(37);
+            bst.Insert(3);
+            bst.Insert(99);
+            bst.Insert(22);
+
+            Console.WriteLine("InOrder traversal:");
+            bst.InOrder(bst.RootNode);
+
+            Console.WriteLine($"Value exists? {bst.Exists(2)}");
+
+
+        }
+    }
+}
