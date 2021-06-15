c4017be467069f37987c30f032014d61ff55e9e5
Martin Dimitrov <MDI@milestone.dk>
2021-06-15 18:55:21 +0300

    Added Shell sort algorithm

---
+++ Agorithms/Sorting/ShellSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ShellSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            array[0] = 30;
+            array[1] = 24;
+            array[2] = 14;
+            array[3] = 99;
+            array[4] = 1;
+            array[5] = 3;
+
+            array = ShellSort.Sort(array);
+
+            Console.WriteLine("Sorted array:");
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
