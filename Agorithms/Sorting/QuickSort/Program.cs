6cdd96dda95bdbdcf8895f4a4c1a42a54332b364
Martin Dimitrov <MDI@milestone.dk>
2021-07-13 21:38:19 +0300

    Added QuickSort

---
+++ Agorithms/Sorting/QuickSort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace QuickSort
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+
+            Console.WriteLine("Original array : ");
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+
+            QuickSort.Sort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted array : ");
+
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Agorithms/Sorting/QuickSort/QuickSort.cs
@@ -0,0 +1,58 @@
+﻿/// <summary>
+/// Reference https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-9.php
+/// </summary>
+namespace QuickSort
+{
+    public class QuickSort
+    {
+        public static void Sort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    Sort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    Sort(arr, pivot + 1, right);
+                }
+            }
+
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) 
+                        return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+}
